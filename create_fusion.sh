7aeeb86e3f82015900a4dce1c46445219b277027
mlplab <labpc2019@gmail.com>
2021-09-15 15:32:33 +0900

    change upload method

--- create_fusion.sh
+++ create_fusion.sh
@@ -7,14 +7,14 @@ CMDNAME=`basename $0`
 batch_size=64
 # search_epoch=100
 train_epoch=150
-datasets=("CAVE" "Harvard" "ICVL")
+datasets=("CAVE" "Harvard")
 base_model_name="SpectralFusion"
-block_num=3
+block_nums=(3 4 5 6 7 8 9 10 11 12 13)
 concats=('False' 'True')
 modes=("both" "inputOnly" "outputOnly")
 loss_modes=("fusion" "fusion" "mse")
 start_time=$(date "+%m%d")
-# start_time='0702'
+# start_time='0909'
 
 
 while getopts b:e:d:c:m:bn: OPT
@@ -35,20 +35,22 @@ done
 
 for dataset in $datasets; do
     skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
-    for concat in $concats; do
-        i=1
-        for mode in $modes; do
-            echo $dataset $concat $mode $i $loss_modes[$i]
-            python train_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_modes[$i]
-            python evaluate_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_modes[$i]
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/SpectralFusion
+    for block_num in $block_nums; do
+        for concat in $concats; do
+            i=1
+            for mode in $modes; do
+                echo $dataset $concat $mode $i $loss_modes[$i]
+                python train_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_modes[$i]
+                python evaluate_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_modes[$i]
 
-            model_name=$base_model_name\_0$block_num\_${loss_modes[$i]}\_$mode\_$start_time\_$concat
-            skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
-            mkdir ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
-            cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
-            skicka upload ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar 2021/SpectralFusion/$dataset/ckpt_$start_time/
-            skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
-            let i++
+                model_name=$base_model_name\_0$block_num\_${loss_modes[$i]}\_$mode\_$start_time\_$concat
+                mkdir ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+                cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/$model_name\_output.csv
+                cp ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/
+                skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/SpectralFusion/$model_name/
+                let i++
+            done
         done
     done
 done
--- create_hsiOnly.sh
+++ create_hsiOnly.sh
@@ -34,17 +34,17 @@ done
 
 for dataset in $datasets; do
     skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/HSIHSCNN
     for block_num in $block_nums; do
         for concat in $concats; do
             python train_hsiOnly.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -l $loss_mode
             python evaluate_hsiOnly.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -l $loss_mode
 
             model_name=$base_model_name\_0$block_num\_$loss_mode\_$start_time\_$concat
-            skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
             mkdir ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
-            cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
-            skicka upload ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar 2021/SpectralFusion/$dataset/ckpt_$start_time/
-            skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
+            cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/$model_name\_output.csv
+            cp ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+            skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/HSIHSCNN/$model_name
         done
     done
 done
