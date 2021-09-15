7e9ee2256006ac8e7d255a24cefd28b9bbd4203e
mlplab <labpc2019@gmail.com>
2021-09-15 15:06:03 +0900

    create hsiOnly train code

---
+++ create_hsiOnly.sh
@@ -0,0 +1,50 @@
+# !/usr/bin/zsh
+
+
+CMDNAME=`basename $0`
+
+
+batch_size=64
+# search_epoch=100
+train_epoch=150
+datasets=("CAVE" "Harvard")
+base_model_name="HSIHSCNN"
+block_nums=(3 4 5 6 7 8 9 10 11 12 13)
+concats=('False' 'True')
+loss_mode="mse"
+start_time=$(date "+%m%d")
+# start_time='0909'
+
+
+while getopts b:e:d:c:m:bn: OPT
+do
+    echo "$OPTARG"
+    case $OPT in
+        b) batch_size=$OPTARG ;;
+        e) train_epoch=$OPTARG ;;
+        d) datasets=$OPTARG ;;
+        c) concat=$OPTARG ;;
+        m) model_name=$OPTARG ;;
+        bn) block_num=$OPTARG ;;
+        *) echo "Usage: $CMDNAME [-b batch size] [-e epoch]" 1>&2
+            exit 1;;
+    esac
+done
+
+
+for dataset in $datasets; do
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
+    for block_num in $block_nums; do
+        for concat in $concats; do
+            python train_hsiOnly.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -l $loss_mode
+            python evaluate_hsiOnly.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -l $loss_mode
+
+            model_name=$base_model_name\_0$block_num\_$loss_mode\_$start_time\_$concat
+            skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
+            mkdir ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+            cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+            skicka upload ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar 2021/SpectralFusion/$dataset/ckpt_$start_time/
+            skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/$model_name
+        done
+    done
+done
