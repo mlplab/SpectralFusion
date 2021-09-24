062c5ae31c46f8735d91ace5f283606bb80d427a
mlplab <labpc2019@gmail.com>
2021-09-24 16:18:02 +0900

    CHINKO

--- all_train_sh.sh
+++ all_train_sh.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/zsh
 
 
-zsh create_SOTA.sh -st 0924
-zsh create_fusion.sh -st 0924
-zsh create_hsiOnly.sh -st 0924
+zsh create_SOTA.sh -s 0924
+zsh create_fusion.sh -s 0924
+zsh create_hsiOnly.sh -s 0924
