7083af0f1c370c144907bad91fa6dff1d045b4b7
mlplab <labpc2019@gmail.com>
2021-10-08 16:26:38 +0900

    update create_SOTA

--- create_SOTA.sh
+++ create_SOTA.sh
@@ -12,7 +12,8 @@ model_names=("HSCNN" "HyperReconNet" "DeepSSPrior")
 block_num=9
 concats=('False' 'True')
 loss_mode="mse"
-start_time=$(date "+%m%d")
+# start_time=$(date "+%m%d")
+start_time=1009
 # start_time='0702'
 
 
@@ -37,17 +38,22 @@ for dataset in $datasets; do
     skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
     skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/SOTA
     for concat in $concats; do
-        for model_name in $model_names; do
-            echo $dataset $concat $loss_mode $model_name
-            python train_SOTA.py -e $train_epoch -d $dataset -l $loss_mode -st $start_time -bn $block_num -c $concat -b $batch_size -m $model_name
-            python evaluate_SOTA.py -e $train_epoch -d $dataset -l $loss_mode -st $start_time -bn $block_num -c $concat -b $batch_size -m $model_name
+        for base_model_name in $model_names; do
 
             name_block_num=$(printf %02d $block_num)
-            model_name=$model_name\_$name_block_num\_$loss_mode\_$start_time\_$concat
-            mkdir ../SCI_result/$dataset\_sota_$start_time/$model_name/$model_name\_upload
-            cp ../SCI_result/$dataset\_sota_$start_time/$model_name/output.csv ../SCI_result/$dataset\_sota_$start_time/$model_name/$model_name\_upload
-            skicka upload ../SCI_ckpt/$dataset\_$start_time/all_trained_sota/$model_name.tar 2021/SpectralFusion/$dataset/ckpt_$start_time/SOTA
-            skicka upload ../SCI_result/$dataset\_sota_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/SOTA/$model_name
+            model_name=$base_model_name\_$name_block_num\_$loss_mode\_$concat
+
+            if [ -d ./SCI_result/$dataset\_sota/$model_name ]; then
+                python train_SOTA.py -e $train_epoch -d $dataset -l $loss_mode -st $start_time -bn $block_num -c $concat -b $batch_size -m $model_name
+                python evaluate_SOTA.py -e $train_epoch -d $dataset -l $loss_mode -st $start_time -bn $block_num -c $concat -b $batch_size -m $model_name
+            fi
+
+            upload_model_name=$base_model_name\_$name_block_num\_$loss_mode\_$start_time\_$concat
+            mkdir -p ../SCI_result/$dataset\_sota/$upload_model_name/$upload_model_name\_upload
+            cp ../SCI_result/$dataset\_sota/$model_name/$model_name\_upload/$model_name\_output.csv ../SCI_result/$dataset\_sota/$upload_model_name/$upload_model_name\_upload/$upload_model_name\_output.csv
+            cp ../SCI_result/$dataset\_sota/$model_name/$model_name\_upload/$model_name.tar ../SCI_result/$dataset\_sota/$upload_model_name/$upload_model_name\_upload/$upload_model_name.tar
+            skicka upload ../SCI_result/$dataset\_sota/$upload_model_name/$upload_model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/SOTA/$model_name
+            rm -rf ../SCI/result/$dataset\_sota/$upload_model_name
         done
     done
 done
