7ec9e154d0b0689bb4c66db0627366d6660a6829
mlplab <labpc2019@gmail.com>
2021-10-08 15:18:00 +0900

    upload only trained parameters and results

--- upload_ckpt.sh
+++ upload_ckpt.sh
@@ -3,18 +3,17 @@
 
 CMDNAME=`basename $0`
 
-
-dataset="CAVE"
-start_time="0000"
-
-
-while getopts b:d:c:m:bn: OPT
+start_time=$(date "+%m%d")
+while getopts s: OPT
 do
     echo "$OPTARG"
     case $OPT in
-        d) datasets=$OPTARG ;;
         s) start_time=$OPTARG ;;
         *) echo "Usage: $CMDNAME [-b batch size] [-e epoch]" 1>&2
             exit 1;;
     esac
 done
+
+zsh upload_result_hsi.sh -s $start_time
+zsh upload_result_fusion.sh -s $start_time
+zsh upload_result_SOTA.sh -s $start_time
---
+++ upload_result_fusion.sh
@@ -0,0 +1,55 @@
+# !/usr/bin/zsh
+
+
+CMDNAME=`basename $0`
+
+
+batch_size=64
+# search_epoch=100
+train_epoch=150
+datasets=("CAVE" "Harvard")
+base_model_name="SpectralFusion"
+block_nums=(1 2 3 4 5 6 7 8 9 10 11 12 13)
+concats=('False' 'True')
+modes=("inputOnly")
+loss_modes=("fusion" "fusion" "mse")
+start_time=$(date "+%m%d")
+# start_time='0915'
+
+
+while getopts b:e:d:c:m:bn:s: OPT
+do
+    echo "$OPTARG"
+    case $OPT in
+        b) batch_size=$OPTARG ;;
+        e) train_epoch=$OPTARG ;;
+        d) datasets=$OPTARG ;;
+        c) concat=$OPTARG ;;
+        m) model_name=$OPTARG ;;
+        bn) block_num=$OPTARG ;;
+        s) start_time=$OPTARG ;;
+        *) echo "Usage: $CMDNAME [-b batch size] [-e epoch]" 1>&2
+            exit 1;;
+    esac
+done
+
+
+for dataset in $datasets; do
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/SpectralFusion
+    for block_num in $block_nums; do
+        for concat in $concats; do
+            i=1
+            for mode in $modes; do
+
+                name_block_num=$(printf %02d $block_num)
+                model_name=$base_model_name\_$name_block_num\_${loss_modes[$i]}\_$mode\_$start_time\_$concat
+                mkdir ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+                cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/$model_name\_output.csv
+                cp ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/
+                skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/SpectralFusion/$model_name/
+                let i++
+            done
+        done
+    done
+done
