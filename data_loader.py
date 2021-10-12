e36409651f0d9549a5549cf65646928916743fdd
home_machine <illust.training@gmail.com>
2021-10-13 08:52:31 +0900

    unk

--- data_loader.py
+++ data_loader.py
@@ -82,7 +82,8 @@ class SpectralFusionDataset(torch.utils.data.Dataset):
     def __init__(self, img_path: str, mask_path: str, *args, data_name='CAVE',
                  concat: bool=False, data_key: str='data', transform=None,
                  load_mode: str='mat',
-                 rgb_input: bool=True, rgb_label: bool=True, **kwargs) -> None:
+                 rgb_input: bool=True, rgb_label: bool=True,
+                 rgb_encode: bool=False, **kwargs) -> None:
 
         self.img_path = img_path
         self.data = os.listdir(img_path)
@@ -97,6 +98,7 @@ class SpectralFusionDataset(torch.utils.data.Dataset):
         # self.return_mode = return_mode
         self.rgb_input = rgb_input
         self.rgb_label = rgb_label
+        self.rgb_encode = rgb_encode
         self.rgb_ch = {'CAVE': (26, 16, 9),
                        'Harvard': (26, 16, 9),
                        'ICVL': (26, 16, 9)}
@@ -132,6 +134,9 @@ class SpectralFusionDataset(torch.utils.data.Dataset):
             rgb_label = trans_data[self.rgb_ch[self.data_name], :, :]
         else:
             rgb_label = torch.empty_like(measurement_data)
+        if self.rgb_encode:
+            rgb_input = measurement_data
+            rgb_label = torch.empty_like(rgb_input)
 
         input_data = {'hsi': hsi_data}
         label_data = {'hsi': hsi_label}
@@ -189,3 +194,157 @@ class SpectralFusionEvalDataset(SpectralFusionDataset):
 
     def __len__(self):
         return self.data_len
+
+
+class RGBPreTrainDataloader(torch.utils.data.Dataset):
+
+    def __init__(self, img_path: str, mask_path: str, *args, data_name='CAVE',
+                 concat: bool=False, data_key: str='data', transform=None,
+                 load_mode: str='mat',
+                 rgb_input: bool=True, rgb_label: bool=True, **kwargs) -> None:
+
+        self.img_path = img_path
+        self.data = os.listdir(img_path)
+        self.mask_path = mask_path
+        self.data_len = len(self.data)
+        self.concat = concat
+        self.data_key = data_key
+        self.transforms = transform
+        self.mask_transforms = torchvision.transforms.ToTensor()
+        self.data_name = data_name
+        self.load_mode = load_mode
+        # self.return_mode = return_mode
+        self.rgb_ch = {'CAVE': (26, 16, 9),
+                       'Harvard': (26, 16, 9),
+                       'ICVL': (26, 16, 9)}
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        label_data = nd_data[self.rgb_ch[self.data_name]]
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        return input_data, label_data
+
+    def __len__(self):
+        return self.data_len
+
+
+class RGBPreTrainEvalDataloader(RGBPreTrainDataloader):
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        label_data = nd_data[self.rgb_ch[self.data_name]]
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        return self.data[idx], input_data, label_data
+
+
+class RGBTrainDataloader(torch.utils.data.Dataset):
+
+    def __init__(self, img_path: str, mask_path: str, *args, data_name='CAVE',
+                 concat: bool=False, data_key: str='data', transform=None,
+                 load_mode: str='mat',
+                 rgb_input: bool=True, rgb_label: bool=True, **kwargs) -> None:
+
+        self.img_path = img_path
+        self.data = os.listdir(img_path)
+        self.mask_path = mask_path
+        self.data_len = len(self.data)
+        self.concat = concat
+        self.data_key = data_key
+        self.transforms = transform
+        self.mask_transforms = torchvision.transforms.ToTensor()
+        self.data_name = data_name
+        self.load_mode = load_mode
+        # self.return_mode = return_mode
+        self.rgb_ch = {'CAVE': (26, 16, 9),
+                       'Harvard': (26, 16, 9),
+                       'ICVL': (26, 16, 9)}
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        label_data = nd_data
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        return input_data, label_data
+
+    def __len__(self):
+        return self.data_len
+
+
+class RGBTrainEvalDataloader(RGBPreTrainDataloader):
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        label_data = nd_data
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        return self.data[idx], input_data, label_data
