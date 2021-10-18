5fac0a28045c6df593b77ec8460790ea846977e2
home_machine <illust.training@gmail.com>
2021-10-19 01:41:26 +0900

    add single image dataset

--- data_loader.py
+++ data_loader.py
@@ -189,3 +189,85 @@ class SpectralFusionEvalDataset(SpectralFusionDataset):
 
     def __len__(self):
         return self.data_len
+
+
+class SingleDataset(torch.utils.data.Dataset):
+
+    def __init__(self, img_path: str, mask_path: str, *args, data_name='CAVE',
+                 concat: bool=False, data_key: str='data', transform=None,
+                 load_mode: str='mat',
+                 rgb_input: bool=True, rgb_label: bool=True, **kwargs) -> None:
+
+        self.img_path = img_path
+        self.data = os.listdir(img_path)
+        self.mask_path = mask_path
+        self.data_len = len(self.data)
+        self.concat = concat
+        self.data_key = data_key
+        self.transforms = transform
+        self.mask_transforms = torchvision.transforms.ToTensor()
+        self.data_name = data_name
+        self.load_mode = load_mode
+        # self.return_mode = return_mode
+        self.rgb_input = rgb_input
+        self.rgb_label = rgb_label
+        self.rgb_ch = {'CAVE': 0,
+                       'Harvard': 0
+                       'ICVL': 0}
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        label_data = trans_data[self.rgb_ch[self.data_name], :, :]
+
+        return input_data, label_data
+
+    def __len__(self):
+        return self.data_len
+class SingleEvalDataset(SingleDataset):
+
+    def __getitem__(self, idx: int) -> (dict, dict):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        if self.load_mode == 'mat':
+            mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        elif self.load_mode == 'h5':
+            mat_data = h5py.File(os.path.join(self.img_path, self.data[idx]), 'r')[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+
+        if self.concat is True:
+            input_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            input_data = measurement_data
+        label_data = trans_data[self.rgb_ch[self.data_name], :, :]
+
+        return self.data[idx], input_data, label_data
+
+    def __len__(self):
+        return self.data_len
