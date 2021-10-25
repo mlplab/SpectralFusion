83ebb6c87c653253ed37b2fecf2cb77a7f15b8c2
mlplab <labpc2019@gmail.com>
2021-10-25 13:53:00 +0900

    repair upload file

--- create_rgb_fusion.sh
+++ create_rgb_fusion.sh
@@ -7,12 +7,12 @@ CMDNAME=`basename $0`
 batch_size=64
 # search_epoch=100
 train_epoch=150
-datasets=("CAVE")
+datasets=("Harvard")
 base_model_name="SpectralFusionRGBEncoder"
 block_nums=(1 2 3 4 5 6 7 8 9 10 11 12 13)
 concats=('False')
 modes=("inputOnly")
-conv_modes=("normal")
+conv_modes=("normal" "edsr" "ghost")
 loss_mode="mse"
 start_time=$(date "+%m%d")
 # start_time='0915'
@@ -37,21 +37,21 @@ done
 
 i=1
 for dataset in $datasets; do
-    skicka mkdir 2021/$base_model_name/$dataset/ckpt_$start_time/
-    skicka mkdir 2021/$base_model_name/$dataset/ckpt_$start_time/$base_model_name
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/$base_model_name
     for block_num in $block_nums; do
         for concat in $concats; do
             for mode in $modes; do
                 for conv_mode in $conv_modes; do
-                    python train_rgb.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_mode -cm $conv_mode
-                    python evaluate_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_mode -cm $conv_mode
+                    # python train_rgb.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_mode -cm $conv_mode
+                    python evaluate_rgb_fusion.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -m $base_model_name -md $mode -l $loss_mode -cm $conv_mode
 
                     name_block_num=$(printf %02d $block_num)
-                    model_name=$base_model_name\_$name_block_num\_${loss_modes[$i]}\_$mode\_$start_time\_$concat\_$conv_mode
+                    model_name=$base_model_name\_$name_block_num\_$loss_mode\_$mode\_$start_time\_$concat\_$conv_mode
                     mkdir -p ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
                     cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/$model_name\_output.csv
                     cp ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/
-                    skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/SpectralFusion/$model_name/
+                    skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/$base_model_name/$model_name/
                 done
             done
         done
