533296c821dfe3846902c19c2405a722da81ff0f
mlplab <labpc2019@gmail.com>
2021-11-02 15:51:35 +0900

    UNK3

--- trainer.py
+++ trainer.py
@@ -95,8 +95,6 @@ class Trainer(object):
                 output = self.model(**inputs)
             else:
                 output = self.model(inputs)
-            if isinstance(output, (list, tuple)):
-                output = output[-1]
             if isinstance(labels, dict) and isinstance(output, torch.Tensor):
                 labels = labels['hsi']
             loss = self.criterion(output, labels)
