d71127c9d63a828e92650d4c8cffe82c192a658a
mlplab <labpc2019@gmail.com>
2021-11-15 17:07:51 +0900

    add attention learning

---
+++ create_attention.sh
@@ -0,0 +1,58 @@
+# !/usr/bin/zsh
+
+
+CMDNAME=`basename $0`
+
+
+batch_size=64
+# search_epoch=100
+train_epoch=150
+datasets=("CAVE" "Harvard")
+base_model_name="SSAttention"
+block_nums=()
+for i in {1..13}; do
+    block_nums+=($i)
+done
+concats=('False')
+loss_mode="mse"
+start_time=$(date "+%m%d")
+# start_time='1028'
+# start_time='0915'
+
+
+while getopts b:e:d:c:m:bn:s: OPT
+do
+    echo "$OPTARG"
+    case $OPT in
+        b) batch_size=$OPTARG ;;
+        e) train_epoch=$OPTARG ;;
+        d) datasets=$OPTARG ;;
+        c) concat=$OPTARG ;;
+        m) model_name=$OPTARG ;;
+        bn) block_num=$OPTARG ;;
+        s) start_time=$OPTARG ;;
+        *) echo "Usage: $CMDNAME [-b batch size] [-e epoch]" 1>&2
+            exit 1;;
+    esac
+done
+
+
+for dataset in $datasets; do
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/
+    skicka mkdir 2021/SpectralFusion/$dataset/ckpt_$start_time/$base_model_name
+    for block_num in $block_nums; do
+        for concat in $concats; do
+
+            python train_attention.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -l $loss_mode
+            python evaluate_attention.py -e $train_epoch -d $dataset -st $start_time -bn $block_num -c $concat -b $batch_size -l $loss_mode
+
+            name_block_num=$(printf %02d $block_num)
+            model_name=$base_model_name\_$name_block_num\_$loss_mode\_$start_time\_$concat
+            mkdir -p ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload
+            cp ../SCI_result/$dataset\_$start_time/$model_name/output.csv ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/$model_name\_output.csv
+            cp ../SCI_ckpt/$dataset\_$start_time/all_trained/$model_name.tar ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/
+            skicka upload ../SCI_result/$dataset\_$start_time/$model_name/$model_name\_upload/ 2021/SpectralFusion/$dataset/ckpt_$start_time/$base_model_name/$model_name/
+
+        done
+    done
+done
